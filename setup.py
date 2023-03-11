3a8951319f701a977ec83c98ee4a308e80850f20
ChipMcCallahan <thisischipmccallahan@gmail.com>
2023-03-10 17:56:37 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='hcc_tools',
+    url='https://github.com/ChipMcCallahan/HCCTools',
+    author='Chip McCallahan',
+    author_email='thisischipmccallahan@gmail.com',
+    # Needed to actually package something
+    packages=['hcc_tools'],
+    package_dir={'hcc_tools': 'src'},
+    # Needed for dependencies
+    install_requires=[
+    ],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='LICENSE',
+    description='Assorted tools for working with HybridCC Tools.',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+"""Module for working with HybridCC levels and levelsets."""
